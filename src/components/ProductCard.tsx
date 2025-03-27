5e2a9fdf2328e168854c83a07694007f25dfdef4
Issa Petit <issa.petit@outlook.fr>
2025-03-27 19:55:45 +0100

    Authentification mise en place

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,22 @@
+// import { formatCurrency } from "@/lib/formatters"
+// import {Card, CardDescription, CardFooter, CardHeader, CardTitle} from "./ui/card"
+
+// import { Button } from "./ui/button"
+// import Link from "next/link"
+
+// export function ProductCard({id, name, priceInCents, description, imagePath}: ProductCardProps) {
+//   <Card>
+//     <CardHeader>
+//       <CardTitle>{name}</CardTitle>
+//       <CardDescription>{name}</CardDescription>
+//     </CardHeader>
+//     <CardContent>
+//       <p className="line-clamp-4">{description}</p>
+//     </CardContent>
+//     <CardFooter>
+//       <Button asChild size="lg" className="w-full">
+//         <Link href={`/products/${id}/purchase`}>Purchase</Link>
+//       </Button>
+//     </CardFooter>
+//   </Card>
+// }
